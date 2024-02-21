323fee85eae246fbb257307e4151305e03ccb937
muhammad nasief <nasinasief@gmail.com>
2024-02-21 22:09:00 +0530

    UI complet

--- lib/main.dart
+++ lib/main.dart
@@ -28,22 +28,40 @@ class Splashpage extends StatelessWidget {
           SizedBox(
               width: double.infinity,
               height: double.infinity,
-              child: Image.asset( 
-                "image/SplashpageBG-01-01.png",
+              child: Image.asset(
+                "image/SplashpageBG-01.png",
                 fit: BoxFit.cover,
               )),
           Align(
               alignment: Alignment.bottomCenter,
               child: Container(
-                height: mediaqurey.height*.30,
+                height: mediaqurey.height * .17,
                 width: double.infinity,
-                // color: Colors.amberAccent,
-                child: const Center(
+                color: Colors.amberAccent,
+                child: Center(
                     child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                //    
+                        SizedBox(
+                            height: 30,
+                            width: 120,
+                            child: ElevatedButton(
+                                onPressed: () {}, child: const Text("qwe"))),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        SizedBox(
+                            width: 120,
+                            height: 30,
+                            child: ElevatedButton(
+                                onPressed: () {}, child: const Text("qwe")))
                       ],
-                    )),
+                    ),
+                  ],
+                )),
               )),
         ],
       ),
